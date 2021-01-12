0ba17a99fbd134013413a929e4a06f85c04ab736
ryan <rewong03@gmail.com>
2021-01-12 17:58:47 -0600

    Added cool fade in info on projects page

---
+++ js/animate-project-title.min.b679e2739227b85725015df676fd6ab7cf92e29f9e5367bbe83cbb8f5950b5ad.js
@@ -0,0 +1,5 @@
+$(document).ready(function(){setTimeout(function(){$('.project-title').animate({'opacity':1},2000);},100)
+setTimeout(function(){$('.project-description').animate({'opacity':1},2000);},200)
+setTimeout(function(){$('.project-tech').animate({'opacity':1},2000);},300)
+setTimeout(function(){$('.project-team').animate({'opacity':1},2000);},400)
+setTimeout(function(){$('.project-time').animate({'opacity':1},2000);},500)})
